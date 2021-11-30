203cb34d84ddb969a805401bda0786a2ae1b04e7
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-12-01 02:53:32 +0400

    added news.html, apidata.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 let navLinks = document.querySelectorAll('.nav-list-item')
 navLinks.forEach(item => {
-    console.log(item);
     item.addEventListener('click', ()=>{
         item.classList.toggle('active')
     })
