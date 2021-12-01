05a53675fcbbfc373a91161794a7ef2a33431eb4
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-12-01 20:29:16 +0400

    imported fonts

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 let navLinks = document.querySelectorAll('.nav-list-item')
 navLinks.forEach(item => {
     item.addEventListener('click', ()=>{
-        item.classList.toggle('active')
+        item.classList.add('active')
     })
 })
