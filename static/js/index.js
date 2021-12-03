51bce52684176876d49d665ef130082f6f0e821f
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-12-03 22:04:07 +0400

    work a lot :D

--- static/js/index.js
+++ static/js/index.js
@@ -1,31 +1,31 @@
 let navLinks = document.querySelectorAll('.nav-list-item')
 navLinks.forEach(item => {
-    item.addEventListener('click', ()=>{
-        item.classList.add('active')
+    item.addEventListener('click', () => {
+        item.classList.toggle('active')
     })
 })
-const footer = document.querySelector('.slider-footer')
+// const footer = document.querySelector('.slider-footer')
 const burger = document.querySelector('.burger')
 const mainNavigation = document.querySelector('.main-navigation')
 const ul = document.querySelector('.nav-ul')
 
 
-burger.addEventListener('click', ()=> {
-    // mainNavigation.style.display = 'block'
+burger.addEventListener('click', () => burgerWatcher())
+
+
+function burgerWatcher() {
     burger.style.transform = 'rotate(360deg)'
     burger.style.transition = '.5s ease-in-out'
-    footer.style.display = 'block'
     if (mainNavigation.style.transform) {
         mainNavigation.style.transform = ''
         burger.style.transform = 'rotate(0deg)'
-    }else{
+    } else {
         mainNavigation.style.transform = 'translateX(0%)'
     }
-    document.querySelector('body').addEventListener('click', (e)=> {
+    document.addEventListener('click', (e) => {
         if (e.target !== burger && e.target !== ul) {
             mainNavigation.style.transform = ''
             burger.style.transform = 'rotate(0deg)'
         }
     })
-})
-
+}
