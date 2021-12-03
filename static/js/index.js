aa230bf9b39d74659f30c67af2090d7a094fd86c
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-12-03 14:56:55 +0400

    working on sidenav
    updated some responsive properties

--- static/js/index.js
+++ static/js/index.js
@@ -4,3 +4,28 @@ navLinks.forEach(item => {
         item.classList.add('active')
     })
 })
+const footer = document.querySelector('.slider-footer')
+const burger = document.querySelector('.burger')
+const mainNavigation = document.querySelector('.main-navigation')
+const ul = document.querySelector('.nav-ul')
+
+
+burger.addEventListener('click', ()=> {
+    // mainNavigation.style.display = 'block'
+    burger.style.transform = 'rotate(360deg)'
+    burger.style.transition = '.5s ease-in-out'
+    footer.style.display = 'block'
+    if (mainNavigation.style.transform) {
+        mainNavigation.style.transform = ''
+        burger.style.transform = 'rotate(0deg)'
+    }else{
+        mainNavigation.style.transform = 'translateX(0%)'
+    }
+    document.querySelector('body').addEventListener('click', (e)=> {
+        if (e.target !== burger && e.target !== ul) {
+            mainNavigation.style.transform = ''
+            burger.style.transform = 'rotate(0deg)'
+        }
+    })
+})
+
